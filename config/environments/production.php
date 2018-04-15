9d76f4094673676576687b5966b85e5896abf08f
Thomas Hanson <tom@thomass-mbp.home>
2018-04-15 20:17:30 +0100

    test

--- config/environments/production.php
+++ config/environments/production.php
@@ -1,6 +1,5 @@
 <?php
 var_dump(getenv("DATABASE_URL"));
-var_dump($S_server["DATABASE_URL"]);
 $_dbsettings = parse_url(getenv("DATABASE_URL"));/* Production */
 define('DB_NAME',       trim($_dbsettings["path"],"/"));
 define('DB_USER',       $_dbsettings["user"]             );
@@ -8,7 +7,7 @@ define('DB_PASSWORD', $_dbsettings["pass"]          );
 define('DB_HOST',     $_dbsettings["host"]          );
 define('WP_HOME', getenv('WP_HOME'));
 define('WP_SITEURL', getenv('WP_SITEURL'));
-
+var_dump($_dbsettings);
 ini_set('display_errors', 0);
 define('WP_DEBUG_DISPLAY', false);
 define('SCRIPT_DEBUG', false);
