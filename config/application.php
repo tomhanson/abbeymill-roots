5d628115d48e123593428487952e1aeb8383535d
Thomas Hanson <tom@thomass-mbp.home>
2018-04-15 14:19:21 +0100

    testtt

--- config/application.php
+++ config/application.php
@@ -10,7 +10,8 @@ $webroot_dir = $root_dir . '/web';
  * Expose global env() function from oscarotero/env
  */
 Env::init();
-var_dump($endpoint = $_SERVER['DATABASE_URL']);
+$endpoint = $_SERVER['DATABASE_URL'];
+var_dump($endpoint);
 
 die();
 /**
