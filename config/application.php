71e243aca5a1e93475fbbbc9caf68310935274aa
Thomas Hanson <tom@thomass-mbp.home>
2018-04-15 16:03:38 +0100

    testing

--- config/application.php
+++ config/application.php
@@ -55,6 +55,7 @@ define('DB_HOST', env('DB_HOST') ?: 'localhost');
 define('DB_CHARSET', 'utf8mb4');
 define('DB_COLLATE', '');
 $table_prefix = env('DB_PREFIX') ?: 'wp_';
+
 /**
  * Authentication Unique Keys and Salts
  */
