7414bcd15c0901e136d3515cb37d9c6a55d91aff
Thomas Hanson <tom@thomass-mbp.home>
2018-04-16 21:04:58 +0100

    test

--- config/environments/production.php
+++ config/environments/production.php
@@ -1,17 +1,16 @@
 <?php
 // var_dump(getenv("DATABASE_URL"));
 $_dbsettings = parse_url(getenv("DATABASE_URL"));/* Production */
-define('DB_NAME',       trim($_dbsettings["path"],"/"));
-define('DB_USER',       $_dbsettings["user"]             );
-define('DB_PASSWORD', $_dbsettings["pass"]          );
-define('DB_HOST',     $_dbsettings["host"]          );
-define('WP_HOME', getenv('WP_HOME'));
-define('WP_SITEURL', getenv('WP_SITEURL'));
+define("DB_NAME",       trim($_dbsettings["path"],"/"));
+define("DB_USER",       $_dbsettings["user"]             );
+define("DB_PASSWORD", $_dbsettings["pass"]          );
+define("DB_HOST",     $_dbsettings["host"]          );
+define("WP_HOME", getenv("WP_HOME"));
+define("WP_SITEURL", getenv("WP_SITEURL"));
 // var_dump($_dbsettings);
-var_dump($_server['DB_NAME']);
 var_dump(getenv("WP_HOME"));
 var_dump(getenv("WP_HOME"));
-ini_set('display_errors', 0);
-define('WP_DEBUG_DISPLAY', false);
-define('SCRIPT_DEBUG', false);
-define('DISALLOW_FILE_MODS', true); // this disables all file modifications including updates and update notifications
+ini_set("display_errors", 0);
+define("WP_DEBUG_DISPLAY", false);
+define("SCRIPT_DEBUG", false);
+define("DISALLOW_FILE_MODS", true); // this disables all file modifications including updates and update notifications
