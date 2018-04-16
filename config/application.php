e8f88b2ea16557d36c34e4942b5e7aa6684f8e70
Thomas Hanson <tom@thomass-mbp.home>
2018-04-16 21:44:42 +0100

    test

--- config/application.php
+++ config/application.php
@@ -15,17 +15,16 @@ Env::init();
  * Use Dotenv to set required environment variables and load .env file in root
  */
 $dotenv = new Dotenv\Dotenv($root_dir);
-var_dump($root_dir);
 if (file_exists($root_dir . "/.env")) {
+    var_dump('test');
     $dotenv->load();
     $dotenv->required(["DB_NAME", "DB_USER", "DB_PASSWORD", "WP_HOME", "WP_SITEURL"]);
 }
-var_dump($dotenv);
-var_dump(env());
 /**
  * Set up our global environment constant and load its config first
  * Default: production
  */
+var_dump(env);
 define("WP_ENV", env("WP_ENV") ?: "production");
 
 $env_config = __DIR__ . "/environments/" . WP_ENV . ".php";
