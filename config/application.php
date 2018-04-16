d698c6633cca435548461cf533f8ad8f0ea5c614
Thomas Hanson <tom@thomass-mbp.home>
2018-04-16 21:42:43 +0100

    test

--- config/application.php
+++ config/application.php
@@ -15,6 +15,7 @@ Env::init();
  * Use Dotenv to set required environment variables and load .env file in root
  */
 $dotenv = new Dotenv\Dotenv($root_dir);
+var_dump($root_dir);
 if (file_exists($root_dir . "/.env")) {
     $dotenv->load();
     $dotenv->required(["DB_NAME", "DB_USER", "DB_PASSWORD", "WP_HOME", "WP_SITEURL"]);
